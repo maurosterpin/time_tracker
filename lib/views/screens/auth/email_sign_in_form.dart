09c6177f9f5c09282e2e6725695f5c9e0e2d6e57
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-02-09 09:41:04 +0100

    email sign in form text fields

--- lib/views/screens/auth/email_sign_in_form.dart
+++ lib/views/screens/auth/email_sign_in_form.dart
@@ -11,13 +11,23 @@ class EmailSignInForm extends StatelessWidget {
           hintText: 'test@test.com'
         ),
       ),
+      TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+      ),
     ];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
     );
   }
 }
