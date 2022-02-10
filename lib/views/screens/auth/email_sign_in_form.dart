dc468e7225b12268315f6d06440f7cb185785b25
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-02-10 16:07:15 +0100

    FocusNode & FocusScope

--- lib/views/screens/auth/email_sign_in_form.dart
+++ lib/views/screens/auth/email_sign_in_form.dart
@@ -16,6 +16,8 @@ class EmailSignInForm extends StatefulWidget {
 class _EmailSignInFormState extends State<EmailSignInForm> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
 
   String get _email => _emailController.text;
   String get _password => _passwordController.text;
@@ -32,6 +34,10 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
     } catch (e) {
       print(e.toString());
     }
+  }
+
+  void _emailEditingComplete() {
+    FocusScope.of(context).requestFocus(_passwordFocusNode);
   }
 
   void _toggleFormType() {
@@ -63,17 +69,20 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
   TextField _buildPasswordTextField() {
     return TextField(
       controller: _passwordController,
+      focusNode: _passwordFocusNode,
       obscureText: true,
       decoration: InputDecoration(
         labelText: 'Password',
       ),
       textInputAction: TextInputAction.done,
+      onEditingComplete: _submit,
     );
   }
 
   TextField _buildEmailTextField() {
     return TextField(
       controller: _emailController,
+      focusNode: _emailFocusNode,
       decoration: InputDecoration(
         labelText: 'Email',
         hintText: 'test@test.com'
@@ -81,6 +90,7 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
       autocorrect: false,
       keyboardType: TextInputType.emailAddress,
       textInputAction: TextInputAction.next,
+      onEditingComplete: _emailEditingComplete,
     );
   }
 
